1977d6bb203f9515c16e9011c62fdc9c5488bd8c
lee <liwenqing2008@126.com>
2019-01-05 20:32:11 +0800

    first commint

--- okhttp/src/test/java/com/example/okhttp/OkhttpApplicationTests.java
+++ okhttp/src/test/java/com/example/okhttp/OkhttpApplicationTests.java
@@ -1,16 +1,58 @@
 package com.example.okhttp;
 
+import com.example.tester.TestPrintLog;
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = TestPrintLog.class)
 public class OkhttpApplicationTests {
+
+    @Autowired
+    TestPrintLog testClass;
 
     @Test
     public void contextLoads() {
+        testClass.print("hello");
+    }
+
+    @Test
+    public void contextLoads2() {
+        testClass.print("hello");
+    }
+
+    @Test
+    public void testTwo(){
+        System.out.println("test hello 2");
+        TestCase.assertEquals(1, 1);
+    }
+
+//    @Test
+//    public void testWrong(){
+//        System.out.println("test hello 2");
+//        TestCase.assertEquals(1, 2);
+//    }
+
+    @Test
+    public void testReturn(){
+        System.out.println("test return int 1");
+        TestCase.assertEquals(1, testClass.return1());
+    }
+
+    @Before
+    public void testBefore(){
+        System.out.println("before");
+    }
+
+    @After
+    public void testAfter(){
+        System.out.println("after");
     }
 
 }
---
+++ tester/src/main/java/com/example/tester/TestPrintLog.java
@@ -0,0 +1,11 @@
+package com.example.tester;
+
+public class TestPrintLog {
+    public void print(String str){
+        System.out.println("print ......" + str);
+    }
+
+    public int return1(){
+        return 1;
+    }
+}
